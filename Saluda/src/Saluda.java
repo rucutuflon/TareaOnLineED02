de2655fd5aa4df0d88cda3f2a9025d7b32f18406
rucutuflon <rucutuflon@hotmail.com>
2016-11-29 13:57:04 +0100

    Saludo personal

--- Saluda/src/Saluda.java
+++ Saluda/src/Saluda.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 
 public class Saluda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Un saludo");
+		Scanner entrada = new Scanner (System.in);
+		String nom;
+		System.out.println("¿Como te llamas?");
+		nom = entrada.next();
+		System.out.println("Un saludo "+nom);
 	}
 
 }
