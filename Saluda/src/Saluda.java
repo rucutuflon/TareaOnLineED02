99f314b4ad517771cd51b26dd7cf0f53e0fef791
rucutuflon <rucutuflon@hotmail.com>
2016-12-01 10:29:40 +0100

    Creamos la variable de clase _nombre

--- Saluda/src/Saluda.java
+++ Saluda/src/Saluda.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class Saluda {
 
+	private static String _nombre;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner entrada = new Scanner (System.in);
-		String nom;
 		System.out.println("¿Como te llamas?");
-		nom = entrada.next();
-		System.out.println("Un saludo "+nom);
+		_nombre = entrada.nextLine();
+		System.out.println("Un saludo "+_nombre);
+		
+		
 	}
 
 }
