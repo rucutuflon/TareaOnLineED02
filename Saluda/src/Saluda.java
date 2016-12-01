ed0580a41375993ce7111046917336df5cc65635
rucutuflon <rucutuflon@hotmail.com>
2016-12-01 10:33:41 +0100

    Encapsulamos la variable de clase _nombre

--- Saluda/src/Saluda.java
+++ Saluda/src/Saluda.java
@@ -10,9 +10,18 @@ public class Saluda {
 		Scanner entrada = new Scanner (System.in);
 		System.out.println("¿Como te llamas?");
 		_nombre = entrada.nextLine();
-		System.out.println("Un saludo "+_nombre);
+		System.out.println("Un saludo "+getNombre());
+		setNombre ("NO SE");
+		System.out.println("He cambiado el nombre a "+getNombre());
 		
-		
+	}
+	
+	public static String getNombre () {
+		return _nombre;
+	}
+	
+	public static void setNombre (String nombre) {
+		_nombre = nombre;
 	}
 
 }
